144e47ebc96b1779b0b5a7228af2ca6e6f5b8620
Frankie Jones <francesca-jones@hotmail.co.uk>
2020-06-02 17:36:43 +0100

    second commit

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ app.use(bodyParser.json());
 // const getWeather = require('./lib/openWeatherApp'); // because i have imported this single function into index.js i can import functions 
 const openWeatherApp = require('./lib/openWeatherApp'); // i have imported the whole file so i can import multiple fucntions using syntac below
 // syntax to use functions for imported files is ie openWeatherMap.getWeather();
-const harryPotter = require('./lib/harryPotter');
-
 
 app.use(express.static(path.join(__dirname, 'public'))) //this shortens the path to the public folder tu use whats indie of it.
 
@@ -21,9 +19,6 @@ app.engine('.hbs', hbs({
 // this creates a template layout for the engine to use
 app.set('view engine', '.hbs')
 
-
-
-
 app.get ('/', async(req,res) => {
     res.render('index')
 });
@@ -31,28 +26,28 @@ app.get ('/', async(req,res) => {
 app.post ('/', async(req,res) => {
     let city = req.body.city
     let data = await openWeatherApp.getWeather(city);
+    console.log(data)
     // let weatherInfo = {
     //     temp:data.main.temp,
     //     name: data.name,
     //     description: data.weather[0].description
     // }
-
     let temp = data.main.temp
-    let name=  data.name
-    let description =  data.weather[0].description
-    let tempCel = Math.ceil(temp-273.15)
-    res.render('index', {data: {temp, name, description, tempCel} })
+    let name = data.name
+    let description = data.weather[0].description
+    // let tempCel = Math.ceil(temp-273.15)
+    res.render('index', {name, description, temp} )
 });
 
 
 
-app.get ('/harrypotter', async(req,res) => {
-    let data = await harryPotter.sortingHat();
-    console.log(data)
-    res.render('harrypotter', {datas})
+
+
+app.get('*', (req,res) => {
+    res.render('404', {
+        message: "this page does not exist, please go back to the previous page"
+    });
 });
-
-
 
 app.listen(3000, () => {
     console.log("listening on port 3000")
